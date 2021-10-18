1b14a08e9973c2eff2551330487f2722ff551226
shahab khodayar <Sh007kh@gmail.com>
2021-10-19 00:09:06 +0330

    add reviews js

--- 3-reviews/app.js
+++ 3-reviews/app.js
@@ -29,3 +29,53 @@ const reviews = [
     text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.getElementById("surprise");
+
+let currentItem = 0;
+
+// load info
+window.addEventListener("DOMContentLoaded", function () {
+  showPerson();
+});
+
+// showPerson
+function showPerson() {
+  item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+nextBtn.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson();
+});
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = 3;
+  }
+  showPerson();
+});
+randomBtn.addEventListener("click", function () {
+  currentItem = randomGenerator();
+  showPerson();
+  console.log("OxO");
+});
+
+// random generator
+function randomGenerator() {
+  let randomNumber = Math.floor(Math.random() * reviews.length);
+  console.log(randomNumber);
+  return randomNumber;
+}
