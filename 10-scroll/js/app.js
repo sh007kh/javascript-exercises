cdebbfc956aeca5b3f5d184f096ae48a0fe5a2f9
Shahab khodayar <sh007kh@gmail.com>
2021-10-29 23:11:28 +0330

    add scroll basic setup files

---
+++ 10-scroll/js/app.js
@@ -0,0 +1,10 @@
+// ********** set date ************
+// select span
+// ********** close links ************
+
+// ********** nav toggle ************
+// select button and links
+// ********** fixed navbar ************
+
+// ********** smooth scroll ************
+// select links
