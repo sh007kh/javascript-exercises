be8560e18e8415ce7ed4b236d7c9869e59770b1d
Shahab khodayar <sh007kh@gmail.com>
2021-10-30 16:15:36 +0330

    add nav toggle function in js

--- 10-scroll/js/app.js
+++ 10-scroll/js/app.js
@@ -1,10 +1,25 @@
 // ********** set date ************
 const date = document.querySelector("#date");
 date.innerHTML = new Date().getFullYear();
-// ********** close links ************
-
 // ********** nav toggle ************
 // select button and links
+
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".link-container");
+const links = document.querySelector(".nav-links");
+const linksHeight = links.getBoundingClientRect().height;
+console.log(linksHeight);
+
+navToggle.addEventListener("click", function () {
+  if (linksContainer.getBoundingClientRect().height === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    // ********** close links ************
+
+    linksContainer.style.height = 0;
+  }
+});
+
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
