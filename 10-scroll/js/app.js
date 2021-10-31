bd7f7c9726436c26800b7482979dc6748c658bf2
shahab khodayar <Sh007kh@gmail.com>
2021-10-31 15:39:41 +0330

    add topLink function in js

--- 10-scroll/js/app.js
+++ 10-scroll/js/app.js
@@ -8,25 +8,6 @@ const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".link-container");
 const links = document.querySelector(".nav-links");
 const linksHeight = links.getBoundingClientRect().height;
-console.log(linksHeight);
-
-navToggle.addEventListener("click", function () {
-  if (linksContainer.getBoundingClientRect().height === 0) {
-    linksContainer.style.height = `${linksHeight}px`;
-  } else {
-    // ********** close links ************
-
-    linksContainer.style.height = 0;
-  }
-});
-
-// ********** fixed navbar ************
-
-const navToggle = document.querySelector(".nav-toggle");
-const linksContainer = document.querySelector(".link-container");
-const links = document.querySelector(".nav-links");
-const linksHeight = links.getBoundingClientRect().height;
-console.log(linksHeight);
 
 navToggle.addEventListener("click", function () {
   if (linksContainer.getBoundingClientRect().height === 0) {
@@ -41,14 +22,23 @@ navToggle.addEventListener("click", function () {
 // ********** fixed navbar ************
 const nav = document.querySelector("#nav");
 const navHeight = nav.getBoundingClientRect().height;
-console.log(navHeight);
 window.addEventListener("scroll", function () {
   if (window.scrollY > navHeight) {
-    console.log(this.window.scrollY);
     nav.classList.add("fixed-nav");
   } else {
     nav.classList.remove("fixed-nav");
-    console.log("removed");
+  }
+});
+// ********** top link ************
+const topLink = document.querySelector(".top-link");
+const heroHeight = document
+  .querySelector(".hero")
+  .getBoundingClientRect().height;
+window.addEventListener("scroll", function () {
+  if (window.scrollY > heroHeight) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
   }
 });
 // ********** smooth scroll ************
