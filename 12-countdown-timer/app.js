ae7d22a1e041c3ed2c756462378f9f1735d536bd
shahab khodayar <Sh007kh@gmail.com>
2021-11-09 23:20:22 +0330

    add giveaway js

--- 12-countdown-timer/app.js
+++ 12-countdown-timer/app.js
@@ -21,3 +21,18 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const deadlineFormat = document.querySelectorAll(".deadline-format h4");
+
+const giveAwayDate = new Date(2022, 4, 25, 11, 30, 0);
+console.log(giveAwayDate);
+const year = giveAwayDate.getFullYear();
+const month = months[giveAwayDate.getMonth()];
+const day = giveAwayDate.getDate();
+const weekDay = weekdays[giveAwayDate.getDay()];
+const hour = giveAwayDate.getHours();
+const minutes = giveAwayDate.getMinutes();
+const seconds = giveAwayDate.getSeconds();
+console.log(seconds);
+giveaway.textContent = `giveaway ends on ${weekDay},${day} ${month} ${year}, ${hour}:${minutes}am `;
