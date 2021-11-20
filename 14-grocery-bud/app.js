68395d7e5a1eb027a099e6cabba3474966a8a195
shahab khodayar <sh007kh@gmail.com>
2021-11-20 23:47:35 +0330

    select elements in grocery JS

--- 14-grocery-bud/app.js
+++ 14-grocery-bud/app.js
@@ -1,7 +1,15 @@
 // ****** SELECT ITEMS **********
-
+const alert = document.querySelector(".alert");
+const groceryForm = document.querySelector(".grocery");
+const groceryContainer = document.querySelector(".grocery-container");
+const groceryList = document.querySelector(".grocery-list");
+const grocery = document.querySelector("#grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const clearBtn = document.querySelector(".clear-btn");
 // edit option
-
+let editElement;
+let editFlag = false;
+let editID = "";
 // ****** EVENT LISTENERS **********
 
 // ****** FUNCTIONS **********
