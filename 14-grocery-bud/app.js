9bc7c9b1f66a1bc0f31e128f4b2e6d753b83fd4c
shahab khodayar <sh007kh@gmail.com>
2021-11-22 23:22:48 +0330

    add alert and add function in grocery js

--- 14-grocery-bud/app.js
+++ 14-grocery-bud/app.js
@@ -11,9 +11,53 @@ let editElement;
 let editFlag = false;
 let editID = "";
 // ****** EVENT LISTENERS **********
-
+groceryForm.addEventListener("submit", addItem);
 // ****** FUNCTIONS **********
-
+groceryList.addE;
+// add function
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+  if (value && !editFlag) {
+    const element = document.createElement("article");
+    let attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.classList.add("grocery-item");
+    element.innerHTML = `
+    <p class="grocery-list-item">${value}</p>
+              <div class="btn-container">
+                <button class="edit-btn">
+                  <i class="fas fa-edit"></i>
+                </button>
+                <button class="delete-btn">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </div>
+    `;
+    // append item
+    groceryList.append(element);
+    console.log(groceryList);
+    addAlert("item added to list", "add");
+    // show container
+    groceryContainer.classList.add("show-container");
+  } else if (value && editFlag) {
+    console.log("editing");
+  } else {
+    addAlert("please enter an item", "remove");
+  }
+}
+// alert function
+function addAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  //   remove alert
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
