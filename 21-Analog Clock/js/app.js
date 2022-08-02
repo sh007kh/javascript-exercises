3b47ef5823ddf92e95619ca185e530d558842a79
shahab khodayar <sh007kh@gmail.com>
2022-08-02 11:37:08 +0430

    [Clock] add js into app.js

--- 21-Analog Clock/js/app.js
+++ 21-Analog Clock/js/app.js
@@ -9,13 +9,17 @@ let minutesCounter = 0;
 let hourCounter = 0;
 
 function getTime() {
+  // -90 deg because of starting point
+  // circle 360 deg / 60 minute and seconds = 6 deg per second and minute
   const seconds = new Date().getSeconds();
   secondCounter = seconds * 6 - 90;
 
   const minutes = new Date().getMinutes();
   minutesCounter = minutes * 6 - 90;
 
+  // circle 360 deg / 12 hours = 30 deg per hour
   let hours = new Date().getHours();
+  // change 24 hours into 12 hours
   if (hours > 12) {
     hours -= 12;
   }
