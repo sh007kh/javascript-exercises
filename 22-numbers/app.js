3ad7c40c6671d187b8ada2d38df9c068e4140bc5
shahab khodayar <sh007kh@gmail.com>
2022-08-03 22:12:26 +0430

    [numbers increment] change underlines to circles, add progressbar js in app.js

--- 22-numbers/app.js
+++ 22-numbers/app.js
@@ -1,12 +1,12 @@
 // variables
 const numbers = [...document.querySelectorAll(".number")];
-const underlines = [...document.querySelectorAll(".underline")];
+const circles = [...document.querySelectorAll(".progress-bar")];
 
 // increment numbers
 numbers.forEach((number) => {
   const initialValue = number.dataset.value;
   let currValue = parseInt(number.textContent);
-  const incrementValue = Math.ceil(initialValue / 250);
+  const incrementValue = Math.ceil(initialValue / 200);
   const counter = setInterval(() => {
     currValue += incrementValue;
     number.textContent = `${currValue}+`;
@@ -17,17 +17,25 @@ numbers.forEach((number) => {
   }, 5);
 });
 
-// increment width of text underline
-underlines.forEach((underline) => {
-  const initialWidth = Math.floor(underline.getBoundingClientRect().width);
-  let currWidth = 0;
-  const incrementWidth = Math.ceil(initialWidth / 500);
+// increment width of progressbar
+const colors1 = ["orange", "lightseagreen", "#221E22"];
+const colors2 = ["yellow", "cyan", "#4E4E4E"];
+const colors3 = ["lightgreen", "dodgerblue", "#B8B8B8"];
+const colors4 = ["green", "teal", "#257D00"];
+
+circles.forEach((circle, index) => {
+  circle.style = `background: radial-gradient(white 50%, transparent 51%),
+    conic-gradient(transparent 0deg 0deg, gainsboro 0deg 360deg),
+    conic-gradient(${colors1[index]} 0deg, ${colors2[index]} 90deg, ${colors3[index]} 180deg, ${colors4[index]});`;
+
+  let deg = 0;
   const counter = setInterval(() => {
-    currWidth += incrementWidth;
-    underline.style.width = `${currWidth}px`;
-    if (currWidth > initialWidth) {
-      underline.style.width = `${initialWidth}px`;
+    deg += 2;
+    circle.style = `background: radial-gradient(white 50%, transparent 51%),
+    conic-gradient(transparent 0deg ${deg}deg, gainsboro ${deg}deg 360deg),
+    conic-gradient(${colors1[index]} 0deg, ${colors2[index]} 90deg, ${colors3[index]} 180deg, ${colors4[index]});`;
+    if (deg === 360) {
       clearInterval(counter);
     }
-  }, 5);
+  }, 1);
 });
